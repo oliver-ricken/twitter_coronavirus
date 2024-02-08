6510ba64febd7d88090b6ccd0855985bd45872ab
Oliver Ricken <2oliver.ricken@gmail.com>
2024-02-08 13:29:08 -0800

    modified map.py, added country tracking

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -57,12 +58,25 @@ with zipfile.ZipFile(args.input_path) as archive:
                 # convert text to lower case
                 text = tweet['text'].lower()
 
+                # set lang var
+                lang = tweet['lang']
+                # check for country code
+                if tweet['place']:
+                    if tweet['place']['country_code']:
+                        country = tweet['place']['country_code']
+                    else:
+                        country = None
+                else:
+                    country = None
                 # search hashtags
                 for hashtag in hashtags:
-                    lang = tweet['lang']
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        if country is not None:
+                            counter_country[hashtag][country] += 1
                     counter_lang['_all'][lang] += 1
+                    if country is not None:
+                        counter_country['_all'][country] += 1
 
 # open the outputfile
 try:
@@ -72,7 +86,12 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
-print('saving',output_path_lang)
-with open(output_path_lang,'w') as f:
-    f.write(json.dumps(counter_lang))
+output_path_country = output_path_base+'.country'
 
+print('saving', output_path_lang)
+with open(output_path_lang, 'w') as f_lang:
+    f_lang.write(json.dumps(counter_lang))
+
+print('saving', output_path_country)
+with open(output_path_country, 'w') as f_country:
+    f_country.write(json.dumps(counter_lang))
